533becd4d1a130015ebd92b75d1607d651aee727
Chris Woolum <chris@turnoutt.com>
2020-07-28 11:22:47 -0700

    Additional logic for handling links

--- TurnouttCapacitorFirebaseDynamicLinks.podspec
+++ TurnouttCapacitorFirebaseDynamicLinks.podspec
@@ -1,7 +1,7 @@
 
   Pod::Spec.new do |s|
     s.name = 'TurnouttCapacitorFirebaseDynamicLinks'
-    s.version = '0.2.1'
+    s.version = '0.2.2'
     s.summary = 'Capacitor Plugin for Firebase Dynamic Links'
     s.license = 'MIT'
     s.homepage = 'https://github.com/Turnoutt/capacitor-firebase-dynamic-links.git'
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -11,222 +11,227 @@ public class CapacitorFirebaseDynamicLinks: CAPPlugin {
         if FirebaseApp.app() == nil {
             FirebaseApp.configure()
         }
-        
+
         DynamicLinks.performDiagnostics(completion: nil)
-                
+
         NotificationCenter.default.addObserver(self, selector: #selector(handleUrlOpened(notification:)), name: Notification.Name(CAPNotifications.URLOpen.name()), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleUniversalLink(notification:)), name: Notification.Name(CAPNotifications.UniversalLinkOpen.name()), object: nil)
     }
-    
+
     @objc func createDynamicLink(_ call: CAPPluginCall) {
-        
         guard let link = URL(string: call.getString("uri")!) else { return }
         let dynamicLinksDomainURIPrefix = call.getString("domainUriPrefix")!
-        
+
         guard let builder = DynamicLinkComponents(link: link, domainURIPrefix: dynamicLinksDomainURIPrefix) else { return }
-        
+
         buildAndroidParameters(call: call, builder: builder)
         buildIOSParameters(call: call, builder: builder)
         buildGoogleAnalyticsParameters(call: call, builder: builder)
         buildITunesParameters(call: call, builder: builder)
         buildSocialMetaParameters(call: call, builder: builder)
-        
+
         guard let longUrl = builder.url else { return }
         print("The long URL is: \(longUrl.absoluteString)")
-        
+
         guard let longDynamicLink = builder.url else { return }
         print("The long URL is: \(longDynamicLink)")
-        
+
         call.success([
-            "value": longDynamicLink
+            "value": longDynamicLink,
         ])
     }
-    
+
     @objc func createDynamicShortLink(_ call: CAPPluginCall) {
-        
         guard let url = call.getString("uri") else { return }
         let link = NSURL(string: url as String)
         let dynamicLinksDomainURIPrefix = call.getString("domainUriPrefix")!
-        
+
         guard let builder = DynamicLinkComponents(link: link! as URL, domainURIPrefix: dynamicLinksDomainURIPrefix) else { return }
-        
+
         buildAndroidParameters(call: call, builder: builder)
         buildIOSParameters(call: call, builder: builder)
         buildGoogleAnalyticsParameters(call: call, builder: builder)
         buildITunesParameters(call: call, builder: builder)
         buildSocialMetaParameters(call: call, builder: builder)
-        
+
         print("The long URL is: \(builder.url!.absoluteString)")
-        
-        builder.shorten() { url, warnings, error in
+
+        builder.shorten { url, _, error in
             print("The short URL is: \(String(describing: url))")
-            
+
             guard let url = url, error == nil else {
                 call.error("Unable to create link")
                 return
             }
-            
+
             call.success([
-                "value": (String(describing: url))
+                "value": String(describing: url),
             ])
         }
-        
     }
-    
+
     @objc func handleUrlOpened(notification: NSNotification) {
         guard let object = notification.object as? [String: Any?] else {
             return
         }
-        
+
         if DynamicLinks.dynamicLinks().dynamicLink(fromCustomSchemeURL: object["url"] as! URL) != nil {
             notifyListeners("deepLinkOpen", data: makeUrlOpenObject(object), retainUntilConsumed: true)
         }
     }
-    
+
     @objc func handleUniversalLink(notification: NSNotification) {
         guard let object = notification.object as? [String: Any?] else {
             return
         }
-        
-        DynamicLinks.dynamicLinks().handleUniversalLink(object["url"] as! URL) { dynamiclink, _ in
+
+        guard let url = object["url"] as? NSURL else { return }
+        guard let parsedUrl = URL(string: url.absoluteString!) else { return }
+
+        let response = DynamicLinks.dynamicLinks().handleUniversalLink(parsedUrl) { dynamiclink, _ in
+
             let url = dynamiclink?.url?.absoluteString ?? ""
-            
+
             self.notifyListeners("deepLinkOpen", data: ["url": url], retainUntilConsumed: true)
         }
+
+        if !response {
+            print("Unable to parse dynamic link. Please ensure you have set up Firebase Dynamic Links correctly.")
+        }
     }
-    
+
     func makeUrlOpenObject(_ object: [String: Any?]) -> JSObject {
         guard let url = object["url"] as? NSURL else {
             return [:]
         }
-        
+
         return [
             "url": url.absoluteString ?? "",
         ]
     }
-    
+
     private func buildGoogleAnalyticsParameters(call: CAPPluginCall, builder: DynamicLinkComponents) {
         let googleAnalyticsParameters = call.getObject("googleAnalytics") ?? [:]
-        
+
         if googleAnalyticsParameters.count > 0 {
             let googleAnalyticsParametersBuilder = DynamicLinkGoogleAnalyticsParameters()
-            
+
             if googleAnalyticsParameters["source"] != nil {
                 googleAnalyticsParametersBuilder.source = googleAnalyticsParameters["source"] as? String
             }
-            
+
             if googleAnalyticsParameters["medium"] != nil {
                 googleAnalyticsParametersBuilder.medium = googleAnalyticsParameters["medium"] as? String
             }
-            
+
             if googleAnalyticsParameters["campaign"] != nil {
                 googleAnalyticsParametersBuilder.campaign = googleAnalyticsParameters["campaign"] as? String
             }
-            
+
             if googleAnalyticsParameters["term"] != nil {
                 googleAnalyticsParametersBuilder.term = googleAnalyticsParameters["term"] as? String
             }
-            
+
             if googleAnalyticsParameters["source"] != nil {
                 googleAnalyticsParametersBuilder.content = googleAnalyticsParameters["content"] as? String
             }
-            
+
             builder.analyticsParameters = googleAnalyticsParametersBuilder
         }
     }
-    
+
     private func buildIOSParameters(call: CAPPluginCall, builder: DynamicLinkComponents) {
         let iosParameters = call.getObject("iosParameters") ?? [:]
-        
+
         if iosParameters.count > 0 {
             let iosParameterBuilder = DynamicLinkIOSParameters(bundleID: (iosParameters["bundleId"] as? String)!)
-            
+
             if iosParameters["appStoreId"] != nil {
                 iosParameterBuilder.appStoreID = iosParameters["appStoreId"] as? String
             }
-            
+
             if iosParameters["fallbackUrl"] != nil {
                 iosParameterBuilder.fallbackURL = URL(string: (iosParameters["fallbackUrl"] as? String)!)
             }
-            
+
             if iosParameters["customScheme"] != nil {
                 iosParameterBuilder.customScheme = iosParameters["customScheme"] as? String
             }
-            
+
             if iosParameters["ipadFallbackUrl"] != nil {
                 iosParameterBuilder.iPadFallbackURL = URL(string: iosParameters["ipadFallbackUrl"] as! String)
             }
-            
+
             if iosParameters["ipadBundleId"] != nil {
                 iosParameterBuilder.iPadBundleID = iosParameters["ipadBundleId"] as? String
             }
-            
+
             if iosParameters["minimumVersion"] != nil {
                 iosParameterBuilder.minimumAppVersion = iosParameters["minimumVersion"] as? String
             }
-            
+
             builder.iOSParameters = iosParameterBuilder
         }
     }
-    
+
     private func buildAndroidParameters(call: CAPPluginCall, builder: DynamicLinkComponents) {
         let androidParameters = call.getObject("androidParameters") ?? [:]
-        
+
         if androidParameters.count > 0 {
             let androidParameterBuilder = DynamicLinkAndroidParameters(packageName: androidParameters["packageName"] as! String)
-            
+
             if androidParameters["minimumVersion"] != nil {
                 androidParameterBuilder.minimumVersion = androidParameters["minimumVersion"] as! Int
             }
-            
+
             if androidParameters["fallbackUrl"] != nil {
                 androidParameterBuilder.fallbackURL = URL(string: androidParameters["fallbackUrl"] as! String)
             }
-            
+
             builder.androidParameters = androidParameterBuilder
         }
     }
-    
+
     private func buildITunesParameters(call: CAPPluginCall, builder: DynamicLinkComponents) {
         let iTunesConnectAnalyticsParameters = call.getObject("iTunesConnectAnalytics") ?? [:]
-        
+
         if iTunesConnectAnalyticsParameters.count > 0 {
             let iTunesConnectAnalyticsParameterBuilder = DynamicLinkItunesConnectAnalyticsParameters()
-            
+
             if iTunesConnectAnalyticsParameters["providerToken"] != nil {
                 iTunesConnectAnalyticsParameterBuilder.providerToken = iTunesConnectAnalyticsParameters["providerToken"] as? String
             }
-            
+
             if iTunesConnectAnalyticsParameters["affiliateToken"] != nil {
                 iTunesConnectAnalyticsParameterBuilder.affiliateToken = iTunesConnectAnalyticsParameters["affiliateToken"] as? String
             }
-            
+
             if iTunesConnectAnalyticsParameters["campaignToken"] != nil {
                 iTunesConnectAnalyticsParameterBuilder.campaignToken = iTunesConnectAnalyticsParameters["campaignToken"] as? String
             }
-            
+
             builder.iTunesConnectParameters = iTunesConnectAnalyticsParameterBuilder
         }
     }
-    
+
     private func buildSocialMetaParameters(call: CAPPluginCall, builder: DynamicLinkComponents) {
         let socialMetaParameters = call.getObject("socialMeta") ?? [:]
-        
+
         if socialMetaParameters.count > 0 {
             let socialMetaParameterBuilder = DynamicLinkSocialMetaTagParameters()
-            
+
             if socialMetaParameters["title"] != nil {
                 socialMetaParameterBuilder.title = socialMetaParameters["title"] as? String
             }
-            
+
             if socialMetaParameters["description"] != nil {
                 socialMetaParameterBuilder.descriptionText = socialMetaParameters["description"] as? String
             }
-            
+
             if socialMetaParameters["imageUrl"] != nil {
                 socialMetaParameterBuilder.imageURL = URL(string: (socialMetaParameters["imageUrl"] as? String)!)
             }
-            
+
             builder.socialMetaTagParameters = socialMetaParameterBuilder
         }
     }
